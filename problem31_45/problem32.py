1d70657d8ee501186e2835ff8eec07e457feded7
Wan, Sigeng <43131626+hendrixone@users.noreply.github.com>
2019-06-02 22:52:48 -0500

    Update problem32.py

--- problem31_45/problem32.py
+++ problem31_45/problem32.py
@@ -1,11 +1,19 @@
+'''
+We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1 through 5 pandigital.
+
+The product 7254 is unusual, as the identity, 39 × 186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
+
+Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
+
+HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
+'''
+
 def digit(number):
     return len(str(number))
-
 
 def get_range(len):
     for i in range(10**(4-len), 10**(5-len)):
         yield i
-
 
 def main():
     a = []
