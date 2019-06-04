cedf0c404f022e5e31df4cd06532629f70dd2ad2
Wan, Sigeng <43131626+hendrixone@users.noreply.github.com>
2019-06-03 22:31:10 -0500

    Update function.py

--- function.py
+++ function.py
@@ -1,1 +1,30 @@
+import math
 
+def primes(bound):
+    '''Generate all primes in range bound'''
+    list = [True] * (bound + 1)
+    list[0] ,list[1] = False, False
+    for i in range(int(math.sqrt(len(list)) + 1)):
+        if list[i] == True:
+            for k in range(2, bound / i + 1):
+                list[i*k] = False
+    
+    for i in range(len(list)):
+        if list[i]:
+            yield i
+
+def is_prime(n):
+    '''Return true is n is prime'''
+    for i in range(2, int(math.sqrt(n) + 1)):
+        if n % i == 0:
+            return False
+    return True
+
+def digit(num):
+    '''return all digit in a list'''
+    result = []
+    for i in range(len(str(num))):
+        temp = num % 10
+        num /= 10
+        result.append(temp)
+    return result[::-1]
